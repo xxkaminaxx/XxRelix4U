0c57ed2ea9a23600925cf5a46ff5e180636481ed
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-15 19:35:04 +0000

    added code into view to check if user credentials are valid

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,25 @@ def logout(request):
 
 
 def login(request):
-    #  returns user to login page
-    personal_login_form = PersonalLoginForm()
-    return render(request,
-                  'login.html', {'personal_login_form': personal_login_form})
+    # Return a login page
+    # when info from form is submited create instance of user
+    # (create an account for user)
+    if request.method == "POST":
+        personal_login_form = PersonalLoginForm(request.POST)
+# checks to see if the username and passwords typed in are correct
+        if personal_login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+
+# if user log them in
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                # if incorrect error !
+            else:
+                personal_login_form.add_error(None,
+                                              "Your credentials are incorrect")
+    else:
+        personal_login_form = PersonalLoginForm()
+    return render(request, 'login.html',
+                  {'personal_login_form': personal_login_form})
