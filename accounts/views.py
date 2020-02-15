a7f5bcc9797a23761219f6ebf040b2b7feec7b6e
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-15 18:34:45 +0000

    added form.py and updated several files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from accounts.forms import PersonalLoginForm
 
 # Create your views here.
+
+
+def index(request):
+    # returns the index.html file
+    return render(request, 'index.html')
+
+
+def logout(request):
+    # logs user out
+    auth.logout(request)
+    messages.success(request, "You have logged out.")
+    return redirect(reverse('index'))
+
+
+def login(request):
+    #  returns user to login page
+    personal_login_form = PersonalLoginForm()
+    return render(request,
+                  'login.html', {'personal_login_form': personal_login_form})
