5ca794f5aeb91a8eac3baa41a68c73eacfc446cc
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-21 08:59:33 +0000

    changed testfile in artifacts app so test passes

--- artifacts/tests.py
+++ artifacts/tests.py
@@ -1,13 +1,12 @@
 from django.test import TestCase
-from .models import Artifacts
+from .models import Artifact
 # Create your tests here.
 
 
 class ProductTests(TestCase):
     # Here we'll define the tests that we'll run against our
-    # Product model
-    
+    # Artifact model
 
     def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+        test_name = Artifact(name='Demon Dweller Sword')
+        self.assertEqual(str(test_name), 'Demon Dweller Sword')
