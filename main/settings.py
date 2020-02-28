63794870d4dee99ddb90edb0aca587a7332e3aa4
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-28 20:14:19 +0000

    updated settings to solve deployment error on heroku

--- main/settings.py
+++ main/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['os.getenv("SITE_NAME")']
 
 
 # Application definition
