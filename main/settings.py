b051d49eab89ed3ae6c7723e267336a656f65308
Stephen Kasolo <kasolos@hotmail.co.uk>
2020-02-29 02:19:05 +0000

    styled nav, search box and made soem structural changes in base.html added new font family from google fonts

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("SITE_NAME"), '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get("SITE_NAME"), '127.0.0.1', 'localhost']
 
 
 # Application definition
